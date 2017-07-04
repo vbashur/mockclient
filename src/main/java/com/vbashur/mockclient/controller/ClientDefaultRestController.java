5f4eca85d5a87578b95f3d4597897b3fd5b69eb2
vbashur <vbashur@yandex.ru>
2017-07-04 23:35:50 +0200

    update for auth integration test

--- src/main/java/com/vbashur/mockclient/controller/ClientDefaultRestController.java
+++ src/main/java/com/vbashur/mockclient/controller/ClientDefaultRestController.java
@@ -2,14 +2,19 @@ package com.vbashur.mockclient.controller;
 
 import com.vbashur.mockclient.restauth.RestTemplateFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
+import org.springframework.http.*;
 import org.springframework.http.client.support.BasicAuthorizationInterceptor;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.Serializable;
+import java.util.*;
 
 @RestController
 public class ClientDefaultRestController {
@@ -25,17 +30,42 @@ public class ClientDefaultRestController {
 
     @RequestMapping(value="/auth", method=RequestMethod.GET)
     @ResponseBody
-    public String auth(@RequestParam("target") String target,
-                       @RequestParam("user") String user,
-                       @RequestParam("password") String password) {
+    public ResponseEntity<? extends Serializable> auth(@RequestParam("target") String target,
+                                            @RequestParam("user") String user,
+                                            @RequestParam("password") String password) {
         RestTemplate restTemplate = restTemplateFactory.getObject();
         restTemplate.getInterceptors().add(
                 new BasicAuthorizationInterceptor(user, password));
-        restTemplate.exchange(
-                target,
-                HttpMethod.GET, null, Void.class); //TODO
 
-        return "Request was sent";
+        MultiValueMap<String, String> credentials = new LinkedMultiValueMap();
+        credentials.add("username", "admin");
+        credentials.add("password", "admin");
+        credentials.add("grant_type", "password");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        HttpEntity body = new HttpEntity(credentials, headers);
+
+        return restTemplate.exchange(
+                target,
+                HttpMethod.POST, body, String.class); //TODO
+
+
+    }
+
+    @RequestMapping(value="/resource", method=RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<? extends Serializable> getResource(@RequestParam("target") String target,
+                                                              @RequestParam("token") String token) {
+        RestTemplate restTemplate = restTemplateFactory.getObject();
+        restTemplate.getInterceptors().clear();
+        HttpHeaders headers = new HttpHeaders();
+        headers.put("Authorization", Arrays.asList("Bearer " + token));
+        HttpEntity body = new HttpEntity(headers);
+        return restTemplate.exchange(
+                target,
+                HttpMethod.GET, body, String.class); //TODO
+
 
     }
 }
--- src/test/java/com/vbashur/mockclient/controller/ClientDefaultRestControllerIntegrationTest.java
+++ src/test/java/com/vbashur/mockclient/controller/ClientDefaultRestControllerIntegrationTest.java
@@ -2,14 +2,23 @@ package com.vbashur.mockclient.controller;
 
 
 import com.vbashur.mockclient.ClientMainApplication;
+import io.jsonwebtoken.Jwt;
+import io.jsonwebtoken.Jwts;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.JacksonJsonParser;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.client.HttpClientErrorException;
+
+import java.io.Serializable;
+import java.util.Map;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {ClientMainApplication.class})
@@ -22,7 +31,24 @@ public class ClientDefaultRestControllerIntegrationTest {
     @Test
     public void testAuthSuccess() {
 //TODO fetch URL and credentials from cfg server
-        controller.auth("http://localhost:8091/auth/index", "admin", "admin");
+        ResponseEntity<? extends Serializable> response =  controller.auth("http://localhost:8091/auth/oauth/token", "maingateway", "");
+        Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
+        System.out.println("BODY: " + response.getBody().toString());
+        System.out.println("HEADERS: " + response.getHeaders().toString());
+        JacksonJsonParser parser= new JacksonJsonParser();
+        Map<String, Object> jwtMap = parser.parseMap(response.getBody().toString());
+        String accessToken = jwtMap.get("access_token").toString();
+        System.out.println("TOKEN: " + jwtMap.get("access_token").toString());
+        Assert.assertNotNull(accessToken);
+
+
+        response =  controller.getResource("http://localhost:8092/account/2", accessToken);
+        Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
+        System.out.println("BODY: " + response.getBody().toString());
+        System.out.println("HEADERS: " + response.getHeaders().toString());
+
+
+
     }
 
     @Test(expected = HttpClientErrorException.class)
